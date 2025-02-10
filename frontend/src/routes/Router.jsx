c7feeb5483703e5f239e96608038092726653f64
MD RIYAD KHAN <you@example.com>
2025-02-10 19:47:42 +0600

    router set on browser router with main.jsx with header and footer for page layout first step

---
+++ frontend/src/routes/Router.jsx
@@ -0,0 +1,27 @@
+import { Route, Routes } from 'react-router-dom';
+import Contact from '../pages/Contact';
+import DoctorDetails from '../pages/Doctors/DoctorDetails';
+import Doctors from '../pages/Doctors/Doctors';
+import Home from '../pages/Home';
+import Login from '../pages/Login';
+import Services from '../pages/Services';
+import SignUp from '../pages/SignUp';
+
+const Router = () => {
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/doctors' element={<Doctors />} />
+        <Route path='/doctor/:id' element={<DoctorDetails/>} />
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/register' element={<SignUp/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/services' element={<Services/>}/>
+        
+      </Routes>
+    </>
+  )
+}
+
+export default Router
