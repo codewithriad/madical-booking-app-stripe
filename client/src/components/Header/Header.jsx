093e6d6ef3227c37a1c1ae0a48c337c63c61cc89
MD RIYAD KHAN <you@example.com>
2025-02-11 20:37:42 +0600

    customize menu

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,10 +1,13 @@
 import { CgMenuRight } from "react-icons/cg";
+import { RiCloseLine } from "react-icons/ri";
 import { Link, NavLink } from "react-router-dom";
 
+import { useState } from "react";
 import figureImg from "../../assets/images/avatar-icon.png";
 import logo from "../../assets/images/logo.png";
 
 const Header = () => {
+  const [menu, setMenu] = useState(true);
   const navLinks = [
     {
       path: "/",
@@ -22,9 +25,30 @@ const Header = () => {
       path: "/contact",
       label: "Contact",
     },
-    
-
   ];
+
+  const openMenu = () => {
+    return (
+      <>
+        <ul>
+          {navLinks.map((link, index) => (
+            <li key={index}>
+              <NavLink
+                to={link.path}
+                className={(navClass) =>
+                  navClass.isActive
+                    ? "text-primaryColor text-[16px] leading-7 font-semibold"
+                    : "text-textColor text-[16px] leading-7 font-medium"
+                }
+              >
+                {link.label}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </>
+    );
+  };
   return (
     <>
       <header className="header flex items-center">
@@ -38,7 +62,7 @@ const Header = () => {
             </div>
             {/* {============== navigation ==============} */}
             <nav className="navigation">
-              <ul className="menu flex items-center gap-[2.7rem]">
+              <ul className="menu sm:hidden md:flex items-center gap-[2.7rem]">
                 {navLinks.map((link, index) => {
                   return (
                     <li key={index}>
@@ -57,26 +81,44 @@ const Header = () => {
                 })}
               </ul>
             </nav>
+            {/* {============== user section ==============} */}
+            <div className="flex justify-center items-center gap-4">
+              <Link to="/">
+                <figure className="w-[35px] h-[35px] rounded-full cursor-pointer">
+                  <img
+                    src={figureImg}
+                    alt="user"
+                    className="w-full rounded-full"
+                  />
+                </figure>
+              </Link>
+
+              <Link to="/login">
+                <button className="text-white bg-primaryColor px-6 py-2 h-[44px] font-semibold flex items-center justify-center rounded-[50px] cursor-auto">
+                  Login
+                </button>
+              </Link>
+              {/* =========== Hamburger menu ============ */}
+              <div>
+                <div
+                  className={`cursor-pointer md:hidden sm:flex`}
+                  onClick={() => setMenu(!menu)}
+                >
+                  {menu ? (
+                    <CgMenuRight className="w-7 h-7 transition-transform" />
+                  ) : (
+                    <RiCloseLine className="w-7 h-7 transition-transform" />
+                  )}
+
+                  {!menu && (
+                    <div className="flex justify-center items-center flex-col absolute top-[20px] right-[4rem]">
+                      {openMenu()}
+                    </div>
+                  )}
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
-
-        {/* {============== user section ==============} */}
-        <div className="flex justify-center items-center gap-4">
-          <Link to="/">
-            <figure className="w-[35px] h-[35px] rounded-full cursor-pointer">
-              <img src={figureImg} alt="user" className="w-full rounded-full" />
-            </figure>
-          </Link>
-
-          <Link to="/login">
-            <button className="text-white bg-primaryColor px-6 py-2 h-[44px] font-semibold flex items-center justify-center rounded-[50px] cursor-auto">
-              Login
-            </button>
-          </Link>
-
-          <div className="">
-          <CgMenuRight />
-          </div> 
         </div>
       </header>
     </>
