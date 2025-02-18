2f7073d10541ef9505c8e4400d281475a947015b
MD RIYAD KHAN <you@example.com>
2025-02-18 14:27:46 +0600

    great doctors section complete

--- client/src/components/Faq/Faq.jsx
+++ client/src/components/Faq/Faq.jsx
@@ -1,20 +1,29 @@
+import faqImg from "../../assets/images/faq-img.png";
 import Accordions from "./Accordions";
 
 const Faq = () => {
   return (
     <>
-    <section>
+      <section>
         <div className="container">
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-3">
             {/* Faq Image */}
-            {/* All Faq importing */}
-      <div>
-        <h2 className="heading mb-8">Most question by our beloved patients</h2>
 
-        <Accordions />
-      </div>
+            <figure className="flex justify-center items-center">
+              <img src={faqImg} alt="faq image" />
+            </figure>
+
+            {/* All Faq importing */}
+            <div className="flex justify-center flex-col mt-8 lg:mt-0">
+              <h2 className="heading mb-8">
+                Most question by our beloved patients
+              </h2>
+
+              <Accordions />
+            </div>
+          </div>
         </div>
-    </section>
-      
+      </section>
     </>
   );
 };
